267e6b7b87d4311f7d7e4331c7f2f9cda4b80859
JohnnyKaster <johnnykaster@rocketmail.com>
2019-10-23 01:24:36 +0000

    I finnished my project which was pretty neat. If only I could spell better...

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 $( "document" ).ready(function() {
     $("#button").click(function() {
         var userInput = $(".input").val();
-        $(".output").text(userInput);
-    })
-
-
+        var pigLatinWord = pigLatinizer(userInput);
+        $(".output").text(pigLatinWord);
+    });
 });
+function pigLatinizer(input) {
+    var vowels = ["a", "e", "i", "o", "u"];
+    var firstLetter = input[0];
+    if (vowels.includes(firstLetter)) {
+        return input + "ay"
+    } else {
+        return input.slice(1) + firstLetter + "ay"
+    }
+};
